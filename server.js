4e4247d8c7165011dc29cec9c0275a25a7370384
Sameer Limbu <sameerlimbu31@gmail.com>
2024-05-08 17:57:05 +0100

    Updated the .env

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+
 
 const port = 3000;
 app.use(cors());
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'sameerlimbu',
-  database: 'comparison',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 // Connect to MySQL
